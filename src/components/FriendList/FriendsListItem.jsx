f3c557675673a6f6945429b65f95d9dc84410f7d
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-03-13 03:36:04 +0300

    friendListItem and Transaction css fixed

--- src/components/FriendList/FriendsListItem.jsx
+++ src/components/FriendList/FriendsListItem.jsx
@@ -2,14 +2,12 @@ import css from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline, id }) => {
   return (
-    <li className={css.friendItem}>
-      <div key={id}>
-        <img src={avatar} alt="Avatar" width="48" />
-        <p className={css.friendName}>{name}</p>
-        <p className={`${isOnline ? css.online : css.offline}`}>
-          {isOnline ? 'Online' : 'Offline'}
-        </p>
-      </div>
+    <li className={css.friendItem} key={id}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p className={css.friendName}>{name}</p>
+      <p className={`${isOnline ? css.online : css.offline}`}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
     </li>
   );
 };
